233570fe66c33c738d71b361f0eace669cc96658
Mike Goren <mike@epicodus.com>
2023-09-05 21:04:35 -0700

    fix date comparisons

--- google-calendar.js
+++ google-calendar.js
@@ -22,25 +22,28 @@ async function fetchExistingGoogleEvents({ calendarApi, calendarId }) {
 }
 
 async function addOrUpdateGoogleEvent({ calendarApi, calendarId, existingEvents, event }) {
-  const existingEvent = existingEvents.find(e => new Date(e.start.dateTime).getUTCDate() === new Date(event.start.dateTime).getUTCDate());
+  const existingEvent = existingEvents.find(e => new Date(e.start.dateTime).toLocaleDateString() === new Date(event.start.dateTime).toLocaleDateString());
   if (existingEvent && existingEvent.summary === event.summary) {
-    console.log('Skipping because event already exists', formatDateTimes(event.start.dateTime, event.end.dateTime));
-    console.log(event);
-    return;
+    console.log('Skipping because event already exists');
+    console.log(new Date(existingEvent.start.dateTime).toLocaleDateString(), existingEvent.summary);
   } else if (existingEvent) {
+    console.log('*** UPDATING EVENT ***');
+    console.log('FROM:');
+    console.log(new Date(existingEvent.start.dateTime).toLocaleDateString(), existingEvent.summary);
     await calendarApi.events.update({
       calendarId,
       eventId: existingEvent.id,
       resource: event,
     });
-    console.log(`Event updated: ${existingEvent.htmlLink}`);
-    console.log(event);
+    console.log('TO:');
   } else {
     const response = await calendarApi.events.insert({
       calendarId,
       resource: event,
     });
-    console.log(`Event created: ${response.data.htmlLink}`);
-    console.log(event);
+    console.log('*** NEW EVENT CREATED ***');
+    console.log(response.data.htmlLink);
   }
+  console.log(new Date(event.start.dateTime).toLocaleDateString(), event.summary);
+  console.log('');
 }
